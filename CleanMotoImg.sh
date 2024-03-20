762fd37abed9519117dfee9a36ac44e403e42e6a
Frog <FroggMaster@users.noreply.github.com>
2024-03-20 03:41:07 -0700

    Create CleanMotoImg.sh
    - Initial script commit
    - Uses DDResuce to skip the first 131072 bytes to remove the Motorola header.
    - Uses DDResuce to skip the last 4096 bytes to remove the Motorola footer. 
    - Removes any present sparse chunks if they're present

---
+++ CleanMotoImg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Use DDRescue to remove the Moto header by skipping the first 131072 bytes
+echo Removing the Moto Header from the system.img.raw skipping the first 131072 bytes
+ddrescue -b 512k -i 131072 -o 0 system.img.raw system.img
+echo "Removing Raw Image"
+rm -rf system.img.raw
+echo "Removing Moto Footer"
+# Use DDRescue to remove the Moto header by removing the last 4096 bytes
+ddrescue -s "$(($(stat -c "%s" system.img) - 4096))" system.img system_cleaned.img
+# Old Method using HEAD
+#head -c $(( 4*1024*-1 )) system.img > system_cleaned.img 
+echo "Removing moto Image"
+mv -f system_cleaned.img system.img 
+echo "Removing sparse chunks"
+rm -rf *chunk*
+echo "system.img is ready"
